a581ec05b1e74e7f7c4eb73d81457e963e7ec042
unknown <andrelopespereira@hotmail.com>
2023-08-03 15:38:29 -0300

    Database configured

--- src/main/java/com/andlopper/customerapi/model/Customer.java
+++ src/main/java/com/andlopper/customerapi/model/Customer.java
@@ -1,6 +1,52 @@
 package com.andlopper.customerapi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private Integer phone;
+    private String email;
+
+    public Customer(Long id, String name, Integer phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
---
+++ src/main/java/com/andlopper/customerapi/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.andlopper.customerapi.repository;
+
+import com.andlopper.customerapi.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
