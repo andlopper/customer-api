8294adcaf927074e6d654acd0d37d014ae720eb1
André Pereira <andrelopespereira@hotmail.com>
2023-09-07 15:48:06 -0300

    Estilo da planilha alterada

--- src/main/java/com/andlopper/customer/api/controller/v1/DownloadController.java
+++ src/main/java/com/andlopper/customer/api/controller/v1/DownloadController.java
@@ -2,10 +2,9 @@ package com.andlopper.customer.api.controller.v1;
 
 import com.andlopper.customer.api.controller.v1.response.CustomerResponse;
 import com.andlopper.customer.api.service.CustomerService;
+import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,13 +34,50 @@ public class DownloadController {
         // Cria um novo arquivo Excel usando o Apache POI
         Workbook workbook = new HSSFWorkbook();
         Sheet sheet = workbook.createSheet("Clientes");
+        sheet.setColumnWidth(0, 1000);
+        sheet.setColumnWidth(1, 5000);
+        sheet.setColumnWidth(2, 4000);
+        sheet.setColumnWidth(3, 10000);
+
+        Row header = sheet.createRow(0);
+
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_GREEN.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        headerStyle.setAlignment(HorizontalAlignment.CENTER);
+        headerStyle.setBorderTop(BorderStyle.THIN);
+        headerStyle.setBorderBottom(BorderStyle.THIN);
+        headerStyle.setBorderLeft(BorderStyle.THIN);
+        headerStyle.setBorderRight(BorderStyle.THIN);
+
+        HSSFFont font = ((HSSFWorkbook) workbook).createFont();
+        font.setFontName("Arial");
+        headerStyle.setFont(font);
 
         // Cria o cabeçalho da planilha
-        Row headerRow = sheet.createRow(0);
-        headerRow.createCell(0).setCellValue("ID");
-        headerRow.createCell(1).setCellValue("Nome");
-        headerRow.createCell(2).setCellValue("Telefone");
-        headerRow.createCell(3).setCellValue("E-mail");
+        Cell headerCell = header.createCell(0);
+        headerCell.setCellValue("Id");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(1);
+        headerCell.setCellValue("Nome");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(2);
+        headerCell.setCellValue("Telefone");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(3);
+        headerCell.setCellValue("Email");
+        headerCell.setCellStyle(headerStyle);
+
+        CellStyle rowStyle = workbook.createCellStyle();
+        rowStyle.setAlignment(HorizontalAlignment.CENTER);
+        rowStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        rowStyle.setBorderTop(BorderStyle.THIN);
+        rowStyle.setBorderBottom(BorderStyle.THIN);
+        rowStyle.setBorderLeft(BorderStyle.THIN);
+        rowStyle.setBorderRight(BorderStyle.THIN);
 
         // Preenche os dados dos clientes
         int rowNum = 1;
@@ -51,6 +87,10 @@ public class DownloadController {
             row.createCell(1).setCellValue(customer.getName());
             row.createCell(2).setCellValue(customer.getPhone());
             row.createCell(3).setCellValue(customer.getEmail());
+
+            for (Cell cell : row) {
+                cell.setCellStyle(rowStyle);
+            }
         }
 
         // Escreve o conteúdo do arquivo Excel em um fluxo de bytes
